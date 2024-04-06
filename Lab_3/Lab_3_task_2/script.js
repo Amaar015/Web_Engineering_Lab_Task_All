58652e9f899ae22f8e6dcd6ac2a4afcae1a83bf4
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2024-04-06 22:54:46 +0500

    added lab 3

---
+++ Lab_3/Lab_3_task_2/script.js
@@ -0,0 +1,22 @@
+const form = document.getElementById('form');
+// const username = document.getElementById('username').value;
+const email = document.getElementById('email');
+const detail = document.getElementById('detail');
+
+//Show input error message
+//show success colour
+
+form.addEventListener('submit', addItem);
+var newItem = document.getElementById('username').value;
+
+console.log(newItem)
+
+
+
+//get FieldName
+
+
+//Event Listeners
+function addItem(e){
+    e.preventDefault();
+}  
